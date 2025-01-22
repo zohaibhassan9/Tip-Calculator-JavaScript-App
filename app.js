a25cba76280ea9bf8acdd063efa8d9c74e4bb278
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-22 15:43:36 +0500

    all values from html feched through 'document.getElementById' method.

---
+++ app.js
@@ -0,0 +1,7 @@
+const billAmount = document.getElementById('billAmount');
+const tipPercentage = document.getElementById('tipPercentage');
+const calculateBtn = document.getElementById('calculateBtn');
+const errpr = document.getElementById('error');
+const result = document.getElementById('result');
+
+
